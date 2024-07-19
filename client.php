8fa58391d079bca85d72325827dadf82c58e987d
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2024-07-19 11:03:42 +0300

    localhost delete

--- client.php
+++ client.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo site_url('/chat/wp-content/plugins/Message/style/style_client.css'); ?>">
+    <link rel="stylesheet" href="<?php echo site_url('wp-content/plugins/Message/style/style_client.css'); ?>">
     <title>LiveChat</title>
     <style>
         @keyframes scroll {
