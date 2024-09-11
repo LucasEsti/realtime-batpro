3663324576fde042c53121dea8e01161fbd4b5c0
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2024-09-11 16:37:49 +0300

    style

--- adminserver.php
+++ adminserver.php
@@ -19,7 +19,7 @@ $source = $scheme . '://' . $host . $scriptName . '/';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 <link rel="stylesheet" href="<?php echo $source; ?>direct-messaging/dist/style.css">
     <style>
-        .listPeople {
+        #listPeople {
             overflow-x: scroll;
             height: 75%;
             position: relative;
