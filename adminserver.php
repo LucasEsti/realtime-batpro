9c820033fb7c7440059547c09aadf61f00c389dc
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2024-09-11 16:36:19 +0300

    style

--- adminserver.php
+++ adminserver.php
@@ -19,6 +19,11 @@ $source = $scheme . '://' . $host . $scriptName . '/';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 <link rel="stylesheet" href="<?php echo $source; ?>direct-messaging/dist/style.css">
     <style>
+        .listPeople {
+            overflow-x: scroll;
+            height: 75%;
+            position: relative;
+        }
         /* Styles pour le modal */
 .modal {
     display: none; /* Masquer le modal par défaut */
